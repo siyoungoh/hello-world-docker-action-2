bc3a994055a3c999382b8cd7713fc40d6ead4b64
SiyoungOh <81408668+siyoungoh@users.noreply.github.com>
2024-10-09 09:28:32 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ GREETING="Hello, $INPUT_WHO_TO_GREET!"
 # Use workflow commands to do things like set debug messages
 echo "::notice file=entrypoint.sh,line=7::$GREETING"
 
+# Ensure that $GITHUB_OUTPUT is set, or provide a default path
+if [ -z "$GITHUB_OUTPUT" ]; then
+  GITHUB_OUTPUT="/tmp/github_output.txt"
+fi
+
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
